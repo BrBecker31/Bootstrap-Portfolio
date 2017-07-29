f5609ecdb900f20f0ff68a1d37e16b2c986c32fa
BrBecker31 <Bryan.w.becker@live.com>
2017-07-29 16:34:24 -0700

    Final Draft

---
+++ assets/js/change.js
@@ -0,0 +1,13 @@
+// Initialize affix and add an offset to add affix class on scroll
+
+
+// $(function() {
+// 	$(window).scroll(function() {
+//   	if($(document).scrollTop() > 50) {
+//     	$('nav').addClass('shrink');
+//     }
+//     else {
+//     $('nav').removeClass('shrink');
+//     }
+//   });
+// });
